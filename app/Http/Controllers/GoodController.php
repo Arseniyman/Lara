19c513c64b7479d905a6999ef1f1a5c928622b2c
Arseniy Vybornov <arseniy@localhost.localdomain>
2020-04-21 15:39:51 +0300

    Add view for goods

--- app/Http/Controllers/GoodController.php
+++ app/Http/Controllers/GoodController.php
@@ -8,13 +8,17 @@ use App\Models\Good;
 
 class GoodController extends Controller {
     
-    public function getGoods(Type $var = null)
-    {
-        # code...
+    public function getGoods() {
+        
+        $good = Good::all();
+        return view('good', compact('good'));
     }
 
 
     public function getGoodPage() {
-        return view('goods');
+        
+        $goods = Good::all();
+
+        return view('good');
     }
 }
--- resources/views/support.blade.php
+++ resources/views/support.blade.php
@@ -1,1 +1,7 @@
 @extends('layouts.layout')
+@section('container')
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 ">
+        <h1>В сети полно слухов, что мы предоставляем поддержку. Но на самом деле,
+            ЭТО НЕ ТАК!</h1>
+    </div>
+@endsection
