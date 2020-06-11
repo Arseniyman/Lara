18e9445571c0096fd1708e8c5526cd45b84ce93f
Arseniy <Arseniy-sports@mail.ru>
2020-06-11 06:21:14 +0300

    Fix docker-compose, all work

--- resources/views/good.blade.php
+++ resources/views/good.blade.php
@@ -26,7 +26,7 @@
 @endsection
 
 @section("scriptjs")
-    <script type="text/javascript" src="js/jquery-3.5.1.min.js"></script>
+    <script type="text/javascript" src="public/js/jquery-3.5.1.min.js"></script>
     <script type="text/javascript">
     $.ajaxSetup({
     headers: {
